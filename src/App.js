d78e68d49bc6e20b19add9eb66bdabddf002a449
Amit <rajamitnine@gmail.com>
2022-05-29 12:31:02 +0530

    setup commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div><h1>String</h1><p>Description</p><img src="logo192.png" alt="react"/><h2>Amit Raj</h2><div><h1>Akshay</h1></div></div>
+  )
 }
 
-export default App;
+export default App
